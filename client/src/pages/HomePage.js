daef65ae67b2c8645e021d1163b6735b4f5ecab4
pankajajoshi <joshi.pankaja@gmail.com>
2023-09-04 16:37:15 -0400

    created transection Modal
    added all the data by creteing the form in HomePage
    
    server,controller ,routes for transectionModal

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal, Form, Input, Select } from "antd";
 import Layout from "./../components/Layout/Layout";
 
 const HomePage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  // form handling
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <div className="filters">
+        <div>range filters</div>
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowModal(true)}
+          >
+            Add New
+          </button>
+        </div>
+      </div>
+      <div className="content"></div>
+      <Modal
+        title="Add Transection"
+        open={showModal}
+        onCancel={() => setShowModal(false)}
+        footer={false}
+      >
+        <Form layout="vertical" onFinish={handleSubmit}>
+          <Form.Item label="Amount" name="amount">
+            <Input type="text" />
+          </Form.Item>
+          <Form.Item label="type" name="type">
+            <Select>
+              <Select.Option value="income">Income</Select.Option>
+              <Select.Option value="expense">ExPense</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Category" name="category">
+            <Select>
+              <Select.Option value="salary">Salary</Select.Option>
+              <Select.Option value="bonus">Bonus</Select.Option>
+              <Select.Option value="shares">Shares</Select.Option>
+              <Select.Option value="food">Food</Select.Option>
+              <Select.Option value="shopping">Shopping</Select.Option>
+              <Select.Option value="bills">Bills</Select.Option>
+              <Select.Option value="medical">Medical</Select.Option>
+              <Select.Option value="car">Car</Select.Option>
+              <Select.Option value="tax">Tax</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Date" name="date">
+            <Input type="date" />
+          </Form.Item>
+          <Form.Item label="Refrence" name="refrence">
+            <Input type="type" />
+          </Form.Item>
+          <Form.Item label="Description" name="description">
+            <Input type="type" />
+          </Form.Item>
+          <div className="d-flex justify-content-end">
+            <button type="submit" className="btn btn-primary">
+              Save
+            </button>
+          </div>
+        </Form>
+      </Modal>
     </Layout>
   );
 };
