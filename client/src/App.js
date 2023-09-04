79e83e8a0b7462326f6ba10802fc30a0e140a586
pankajajoshi <joshi.pankaja@gmail.com>
2023-09-04 01:11:06 -0400

    cearted files in pages and added spinner js file
    package json for client and server changed to run using concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
-    <div className="container-fluid">
-      <h1 className="text-center">HEllo</h1>
-    </div>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <HomePage />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
+}
+
+export function ProtectedRoutes(props) {
+  if (localStorage.getItem("user")) {
+    return props.children;
+  } else {
+    return <Navigate to="/login" />;
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-app.get("/", (req, res) => {
-  res.send("<h1>Hello from server</h1>");
-});
-// app.use("/api/v1/users", require("./routes/userRoute"));
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello from server</h1>");
+// });
+app.use("/api/v1/users", require("./routes/userRoute"));
 
 //port
-const PORT = 3000 || process.env.PORT;
+const PORT = 8080 || process.env.PORT;
 
 //listen server
 app.listen(PORT, () => {
