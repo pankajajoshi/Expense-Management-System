bab7f00eaa0ec9b6bde33dfafee4f96686bac84f
Pankaja <joshi.pankaja@gmail.com>
2023-09-12 16:27:19 -0400

    styles for footer and header user added

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -8,7 +8,12 @@ const Footer = () => {
       className="text-dark p-4 d-flex align-items-center justify-content-center fixed-bottom"
       style={{ backgroundColor: "#6f4e37c9" }}
     >
-      <h6 className="text-center mb-0">All rights reserved &copy; PK</h6>
+      <h6
+        className="text-center mb-0"
+        style={{ fontSize: "1.2rem", color: "#7ad4e2" }}
+      >
+        All rights reserved &copy; PK
+      </h6>
       <div className="d-flex ml-2">
         <a
           href="https://github.com/pankajajoshi"
